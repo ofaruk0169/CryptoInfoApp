263e27e8e68fca4df36668a0754f79b82cc7cd38
Omare Faruk <ofaruk0169@gmail.com>
2025-02-10 21:26:40 +0000

    serialized a bunch of values

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,9 @@ android {
 
 dependencies {
 
+    implementation("com.google.code.gson:gson:2.10.1")
+
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
---
+++ app/src/main/java/com/example/cryptoinfoapp/domain/repository/CoinRepository.kt
@@ -0,0 +1,4 @@
+package com.example.cryptoinfoapp.domain.repository
+
+interface CoinRepository {
+}
