937a58df3f91d47368028c1efc61379e7deaa3d8
Omare Faruk <ofaruk0169@gmail.com>
2025-02-11 21:27:30 +0000

    showed up

--- app/src/main/java/com/example/cryptoinfoapp/domain/repository/CoinRepository.kt
+++ app/src/main/java/com/example/cryptoinfoapp/domain/repository/CoinRepository.kt
@@ -1,4 +1,11 @@
 package com.example.cryptoinfoapp.domain.repository
 
+import com.example.cryptoinfoapp.data.remote.dto.CoinDetailDto
+import com.example.cryptoinfoapp.data.remote.dto.CoinDto
+
 interface CoinRepository {
+
+    suspend fun getCoins(): List<CoinDto>
+
+    suspend fun getCoinById(coinId: String): CoinDetailDto
 }
