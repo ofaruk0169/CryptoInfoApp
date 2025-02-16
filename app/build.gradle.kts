344aa799f79ce445bd16f0c060e56d1bd4f55be6
Omare Faruk <ofaruk0169@gmail.com>
2025-02-16 22:40:01 +0000

    D.I complete

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,9 @@ dependencies {
 
     implementation("com.google.code.gson:gson:2.10.1")
 
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0") // Add this line
+
+
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
